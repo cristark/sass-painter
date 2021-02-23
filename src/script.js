62e91382f733950130d052926ffd46f6c2885db2
Cristiano <cristiano.tarconi@gmail.com>
2021-02-24 00:27:48 +0100

    header - second level dropdown + js footer adjs

--- src/script.js
+++ src/script.js
@@ -16,7 +16,11 @@ let app = new Vue({
         wacomDrop: ['Wacom/Painter Offers','Cintiq Pro 16 + Painter 2021','Cintiq 22HD + Painter 2021','Intuos Pro Medium + Painter 2021','Intuos Pro Large + Painter 2021','Cintiq 16 Creative Pen Display + Painter 2021','One Creative Pen Display + Painter 2021','Bamboo Duo Stylus','Wacom Pro Pen 2 with Case'],
         brushesDrop: ['Painter','ParticleShop','Painter Essentials'],
         supportDrop: ['Patches & Updates','Knowledgebase','View all'],
-        learningDrop: ['Tutorials','Tips & Tricks','Artist Interviews','Webinar Library']
+        learningDrop: ['Tutorials','Tips & Tricks','Artist Interviews','Webinar Library'],
+        productsFooter: ['Corel Painter','ParticleShop','Painter Essentials','Special Offers','Free Trials','All products'],
+        quickLinksFooter: ['Licensing','Tutorials & Tips','Webinars','The Painter Factory','Discovery Center','Beta Programs'],
+        supportFooter: ['Register your product','Patches & Updates','Knowledgebase','Uninstall instructions','Contact us'],
+        aboutFooter: ['Company information','Newsroom','Careers','Partner Program','Legal Information','EULA']
 
     }
 });
